df2249c8ebd1de5bd2a45bdfb3bce49ba4a9f5eb
Dima <117664428+BREADushek18@users.noreply.github.com>
2023-02-13 14:32:58 +0700

    Add files via upload

---
+++ Lab1task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+  static void Main()
+  {
+    Console.Write("Введите число: ");
+    double Number = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите степень: ");
+    double Degree = Convert.ToDouble(Console.ReadLine());
+    double Result = 1;
+
+    for (int i = 1; i <= Degree; ++i)
+    {
+      Result *= Number;
+    }
+
+    Console.WriteLine("{0} в степени {1} = {2}.", Number, Degree, Result);
+    Console.ReadKey();
+  }
+}
